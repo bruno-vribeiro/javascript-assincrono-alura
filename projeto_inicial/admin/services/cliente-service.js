dac00b0bdb655ce4c05282bf4c04c344c145eb13
bruno-vribeiro <vilhenab8@gmail.com>
2023-03-16 04:01:10 -0300

    continuando o curso

--- projeto_inicial/admin/services/cliente-service.js
+++ projeto_inicial/admin/services/cliente-service.js
@@ -1,31 +1,31 @@
-const criaNovaLinha = (nome,email) => {
-    const linhaNova = document.createElement('tr');
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-                <td>${email}</td>
-                <td>
-                    <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                    </ul>
-            </td>`
+const listaClientes = () =>{
+//     const promise = new Promise((revolve,reject) => {
+//     const http = new XMLHttpRequest();
 
-            linhaNova.innerHTML = conteudo;
 
-            return linhaNova;
+
+//     http.open('GET','http://localhost:3000/profile');
+
+//     http.onload = () => {
+//         if(http.status>=400){
+//             reject(JSON.parse(http.response))
+//         }else{
+//             revolve(JSON.parse(http.response))
+//         }
+        
+//     }
+//     http.send();
+// })
+// return promise
+
+//API fetch
+return fetch('http://localhost:3000/profile')
+.then(resposta=>{
+    return resposta.json();
+})
 }
 
-const tabela = document.querySelector('[data-tabela]');
-const http = new XMLHttpRequest();
-
-http.open('GET','http://localhost:3000/profile');
-
-http.send();
-
-http.onload = () => {
-    const data = JSON.parse(http.response);
-    data.forEach(element => {
-        tabela.appendChild(criaNovaLinha(element.nome,element.email));
-    });
-    
+export const serviceClientes = {
+    listaClientes
 }
+
